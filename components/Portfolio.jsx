51d2ae4619801abee5f06caaa02e6057c12568e5
felixseeger <felix@super-code.de>
2022-07-21 01:03:46 +0200

    first commit

---
+++ components/Portfolio.jsx
@@ -0,0 +1,63 @@
+import Image from 'next/image';
+import React from 'react';
+
+const Portfolio = () => {
+  return (
+    <div className='max-w-[1240px] mx-auto py-16 text-center'>
+      <h1 className='font-bold text-2xl p-4'>Recent Work</h1>
+      <div className='grid grid-rows-none md:grid-cols-5 p-4 gap-4'>
+        <div className='w-full h-full col-span-2 md:col-span-3 row-span-2'>
+          <Image
+            src='https://wp.wolf-raskin.de/wp-content/uploads/wolf-raskin-zeichnung.jpeg'
+            alt='/'
+            layout='responsive'
+            width='677'
+            height='451'
+          />
+        </div>
+        <div className='w-full h-full'>
+          <Image
+            src='https://wp.wolf-raskin.de/wp-content/uploads/wolf-raskin-offenbar-leinwand_2003.jpeg'
+            alt='/'
+            width='215'
+            height='217'
+            layout='responsive'
+            objectFit='cover'
+          />
+        </div>
+        <div className='w-full h-full'>
+          <Image
+            src='https://wp.wolf-raskin.de/wp-content/uploads/wolf-raskin-collagex4-1080.jpeg'
+            alt='/'
+            width='215'
+            height='217'
+            layout='responsive'
+            objectFit='cover'
+          />
+        </div>
+        <div className='w-full h-full'>
+          <Image
+            src='https://wp.wolf-raskin.de/wp-content/uploads/wolf-raskin_nomad_2021_canvas_120x140cm_acryl-oel.jpeg'
+            alt='/'
+            width='215'
+            height='217'
+            layout='responsive'
+            objectFit='cover'
+          />
+        </div>
+        <div className='w-full h-full'>
+          <Image
+            src='https://wp.wolf-raskin.de/wp-content/uploads/wolf-raskin-Glimp-2015-150x175cm-mixed-Media.jpeg'
+            alt='/'
+            width='215'
+            height='217'
+            layout='responsive'
+            objectFit='cover'
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Portfolio;
---
+++ components/SliderData.js
@@ -0,0 +1,23 @@
+export const SliderData = [
+    {
+      image:
+        'https://wp.wolf-raskin.de/wp-content/uploads/wolf-raskin-zeichnung.jpeg',
+    },
+    {
+      image:
+        'https://wp.wolf-raskin.de/wp-content/uploads/wolf-raskin-offenbar-leinwand_2003.jpeg',
+    },
+    {
+      image:
+        'https://wp.wolf-raskin.de/wp-content/uploads/wolf-raskin_nomad_2021_canvas_120x140cm_acryl-oel.jpeg',
+    },
+    {
+      image:
+        'https://wp.wolf-raskin.de/wp-content/uploads/wolf-raskin-Glimp-2015-150x175cm-mixed-Media.jpeg',
+    },
+    {
+      image:
+        'https://wp.wolf-raskin.de/wp-content/uploads/DSCF0209x4-1800.jpeg',
+    },
+  ];
+  
